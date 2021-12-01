1a4d5e3c72dfebb704c54b15feb98899fb6e8179
Eduardo Humberto <eduardo.humberto1992@gmail.com>
2021-12-01 12:25:22 -0300

    create comment system structure

---
+++ resources/js/components/BlogPostComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <t-modal v-model="showModal">
+            <comment />
+        </t-modal>
+        <button @click="showModal=true" type="button">Show modal</button>
+    </div>
+</template>
+
+<script>
+import Comment from './CommentComponent'
+export default {
+    components: {
+        Comment,
+    },
+    data() {
+        return {
+            showModal: false
+        }
+    }
+}
+</script>
---
+++ resources/js/components/CommentFormComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex items-center">
+        <div class="container mx-auto">
+            <div class="max-w-md mx-auto my-10 bg-white p-5">
+                <div class="text-center">
+                    <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Contact Us</h1>
+                    <p class="text-gray-400 dark:text-gray-400">Fill up the form below to send us a message.</p>
+                </div>
+                <div class="m-7">
+                    <form method="POST" id="form">
+                        <div class="mb-6">
+                            <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Full Name</label>
+                            <input type="text" name="name" id="name" placeholder="John Doe" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+                        </div>
+                        <div class="mb-6">
+                            <label for="message" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Your Message</label>
+
+                            <textarea rows="5" name="message" id="message" placeholder="Your Message" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" required></textarea>
+                        </div>
+                        <div class="mb-6">
+                            <button type="submit" class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Send Message</button>
+                        </div>
+                        <p class="text-base text-center text-gray-400" id="result">
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showModal: false
+        }
+    }
+}
+</script>
